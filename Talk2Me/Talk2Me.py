b43df0f81ee23be75a50e31a9d8eb53f8b596dff
Keon Soltani <keon@berkeley.edu>
2024-10-19 17:03:32 -0700

    persona buttton

--- Talk2Me/Talk2Me.py
+++ Talk2Me/Talk2Me.py
@@ -1,6 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+from .components.personas import personas
 
 from rxconfig import config
 
@@ -13,32 +14,17 @@ def index() -> rx.Component:
         rx.color_mode.button(position="top-right"),
         rx.vstack(
             rx.heading("An innovative tool to improve social comfort.", size="9"),
-            rx.text(
-                "Choose a persona to #Talk2.",
-                size="5",
-            ),
-            rx.link(
-                rx.button("Let's get chatting.", color_scheme="purple", cursor="pointer"),
-                href="/personas",
-                is_external=True,
+            rx.button(
+                "Choose a Persona to Talk2.",
+                on_click=rx.redirect(
+                    "/personas",
+                ),
             ),
             spacing="5",
             justify="center",
             min_height="85vh",
         ),
     )
-
-def personas():
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Personas:", size="9"),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-    )
-
 
 app = rx.App()
 app.add_page(personas, route="/personas")
--- Talk2Me/components/personas.py
+++ Talk2Me/components/personas.py
@@ -1,2 +1,12 @@
 import reflex as rx
 
+def personas():
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Personas:", size="9"),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+    )
