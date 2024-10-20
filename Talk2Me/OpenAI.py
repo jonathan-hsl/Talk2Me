5af5d32fa497b08d59f0ae1a34f021dbdbd4111a
Jonathan Camacho <jonathan.hsl@berkeley.edu>
2024-10-19 17:57:17 -0700

    finished openai api call

--- Talk2Me/OpenAI.py
+++ Talk2Me/OpenAI.py
@@ -1,14 +1,14 @@
 from openai import OpenAI
 client = OpenAI()
 
-def openAIresponse(topic):
+def openai_response(text_contents): # this function is gonna take in the txt file/its contents
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {
                 "role": "user",
-                "content": "Write a haiku about recursion in programming."
+                "content": f"Go through each of the links contained in here: '{text_contents}'. Summarize them and give me detailed notes about them."
             }
         ]
     )
