3b73926b0293e8257b324d875462f98b61f0b809
Keon Soltani <keon@berkeley.edu>
2024-10-19 17:34:02 -0700

    wip personas

--- Talk2Me/server.py
+++ Talk2Me/server.py
@@ -3,4 +3,7 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
     
-    ...
+    persona: str = ""
+    
+    def set_persona(self, persona):
+        self.persona = persona
