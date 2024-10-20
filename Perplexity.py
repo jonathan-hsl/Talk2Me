3f6be3ad0009182d1d9abdb70bb7010345248f01
Jonathan Camacho <jonathan.hsl@berkeley.edu>
2024-10-19 17:32:06 -0700

    added

--- Perplexity.py
+++ Perplexity.py
@@ -2,36 +2,37 @@ from openai import OpenAI
 
 YOUR_API_KEY = "PERPLEXITY_API_KEY"
 
-messages = [
+client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
+
+# chat completion without streaming
+def perplexityResponse(topic):
+    messages = [
     {
         "role": "system",
         "content": (
-            "You are an artificial intelligence assistant and you need to "
-            "engage in a helpful, detailed, polite conversation with a user."
+            f"Help me research {topic}. Give me some links to reputable websites that provide information on {topic}", # HERE, WE HAVE TO PASS IN SOME STANDARDIZED INPUT BASED ON WHATEVER THE USER SELECTS FROM THE DROPDOWN"
         ),
     },
     {
         "role": "user",
         "content": (
-            "How many stars are in the universe?"
+            "How many stars are in the universe?" # THIS HERE IS GOING TO BE THE INPUT TOPIC BASED ON WHATEVER THE USER SELECTS ON THE FRONT END DROPDOWN
         ),
     },
 ]
-
-client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
-
-# chat completion without streaming
-response = client.chat.completions.create(
-    model="llama-3-sonar-large-32k-online",
-    messages=messages,
-)
-print(response)
+    response = client.chat.completions.create(
+        model="llama-3-sonar-large-32k-online",
+        messages=messages,
+    )
+    return response
+    print(response)
 
 # chat completion with streaming
-response_stream = client.chat.completions.create(
+'''response_stream = client.chat.completions.create(
     model="llama-3-sonar-large-32k-online",
     messages=messages,
     stream=True,
 )
 for response in response_stream:
     print(response)
+'''
